231049da3e8ff8693942d7d6c7dce382ee75d8bd
cdent <cdent@bb0f57cd-c710-0410-a8fb-e8bc1be0d679>
2008-10-19 20:02:10 +0000

    tiddlyweb - updates to spowt to make it a) a bit easier to install b) support slightly more complicated authentication structures (see the changes to user.html)
    git-svn-id: http://svn.tiddlywiki.org/Trunk/contributors/ChrisDent/experimental/tiddlyweb-plugins@7247 bb0f57cd-c710-0410-a8fb-e8bc1be0d679

--- spowt/spowt.py
+++ spowt/spowt.py
@@ -12,6 +12,8 @@ from tiddlyweb.store import NoUserError, NoRecipeError, NoBagError
 from tiddlyweb.model.policy import ForbiddenError, UserRequiredError
 from tiddlyweb.web.util import server_base_url, recipe_url
 
+TIDDLYWEB_BAG = 'tiddlyweb'
+
 template_env = Environment(loader=FileSystemLoader('.'))
 
 def spowt(environ, start_response):
@@ -44,17 +46,25 @@ def new_wiki(environ, start_response):
         except NoBagError:
             bag.desc = 'hello'
             bag.policy.owner = username
+            bag.policy.manage = [username]
             if perms == 'closed':
-                bag.policy.manage = [username]
                 bag.policy.read = [username]
                 bag.policy.write = [username]
                 bag.policy.create = [username]
                 bag.policy.delete = [username]
+            if perms == 'authrw':
+                bag.policy.read = ['ANY']
+                bag.policy.write = ['ANY']
+                bag.policy.create = ['ANY']
+                bag.policy.delete = ['ANY']
             if perms == 'read':
-                bag.policy.manage = [username]
                 bag.policy.write = [username]
                 bag.policy.create = [username]
                 bag.policy.delete = [username]
+            if perms == 'authw':
+                bag.policy.write = ['ANY']
+                bag.policy.create = ['ANY']
+                bag.policy.delete = ['ANY']
             store.put(bag)
 
         recipe = Recipe(wikiname)
@@ -65,7 +75,7 @@ def new_wiki(environ, start_response):
             recipe.desc = 'hello'
             recipe.policy.owner = username
             recipe.policy.manage = [username]
-            recipe.set_recipe([['tiddlyweb', ''], [bag.name, '']])
+            recipe.set_recipe([[TIDDLYWEB_BAG, ''], [bag.name, '']])
             store.put(recipe)
 
         user = User(username)
