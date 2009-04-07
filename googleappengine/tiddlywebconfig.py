bc40b0fce7589a5082e41e120d30e4b84f46a320
Chris Dent <cdent@matter.lan>
2009-04-07 19:18:58 +0100

    tiddlyweb - Document the googleappending tiddlywebconfig.py.

--- googleappengine/tiddlywebconfig.py
+++ googleappengine/tiddlywebconfig.py
@@ -1,5 +1,8 @@
 import os
 
+# The following block determine the server host
+# setting. This allows us to work local and on
+# GAE without changing config by hand.
 if os.environ['SERVER_SOFTWARE'].startswith('Dev'):
     server_host = {
             'scheme': 'http',
@@ -14,17 +17,25 @@ else:
             }
 
 config = {
+        # how are we storing data
         'server_store': ['googledata', {}],
+        # what extensions (on urls) can we use in addition
+        # to defaults
         'extension_types': {
             'atom': 'application/atom+xml',
             },
+        # which serializers are used to handle particular
+        # content types (merged with defauls)
         'serializers': {
             'application/atom+xml': ['atom.atom', 'application/atom+xml; charset=UTF-8'],
             'text/html': ['atom.htmlatom', 'text/html; charset=UTF-8'],
             },
+        # how do we extract user information from requests
         'extractors': ['google_user_extractor'],
+        # how do we authenticate (if needed)
         'auth_systems': ['google_user_challenger'],
+        # where is a handy css file located
         'css_uri': '/static/tiddlyweb.css',
-        #'system_plugins': ['twoter'],
+        # what is the host and port of our server
         'server_host': server_host,
         }
