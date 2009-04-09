343601cec92cf8366d5f3691d0f0e102081a63ef
Chris Dent <cdent@matter.local>
2009-04-09 14:39:51 +0100

    tiddlyweb - Use tiddlyweb.appspot.com as the target server for tiddlywebweb.

--- tiddlywebweb/tiddlywebconfig.py
+++ tiddlywebweb/tiddlywebconfig.py
@@ -1,7 +1,7 @@
 config = {
         'log_level': 'DEBUG',
         'server_store': ['tiddlywebweb.tiddlywebstore', {
-            'server_base': 'http://localhost:8000',
+            'server_base': 'http://tiddlyweb.appspot.com',
             #'user': 'somebody', # if you wish to use basic auth
             #'password': 'somepass',
             }],
