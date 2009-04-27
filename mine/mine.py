d098faa0f342c6110f4c55f1166a6cca02c9b6a7
Chris Dent <cdent@matter.lan>
2009-04-27 22:37:39 +0100

    enable bag creation via for the form interface
    This lets the user create a bag based on their requirements.
    
    Note that _bag_exists is a stub that always returns False, this
    should be changed.

--- mine/mine.py
+++ mine/mine.py
@@ -7,8 +7,10 @@ import urllib
 
 from jinja2 import Environment, FileSystemLoader
 
+from tiddlyweb.model.bag import Bag
+from tiddlyweb.model.policy import UserRequiredError, ForbiddenError
 from tiddlyweb.web.http import HTTP400, HTTP302
-from tiddlywebplugins import do_html, entitle
+from tiddlywebplugins import do_html, entitle, require_any_user
 
 
 template_env = Environment(loader=FileSystemLoader('templates'))
@@ -43,10 +45,10 @@ Create and manage <a href='http://tiddlyweb.peermore.com/wiki/#TiddlyWebWiki'>Ti
             message=message)
 
 
+@require_any_user()
 def manage_mine(environ, start_response):
     """Handle POST"""
     # put a try around this stuff eventually
-    logging.debug(environ)
     creation = environ['tiddlyweb.query']['creatego'][0]
     if creation == 'Quick!':
         _quick_create(environ)
@@ -66,7 +68,55 @@ def _create_recipe(environ):
     pass
 
 def _create_bag(environ):
-    pass
+    """Take the form input and turn it into a bag."""
+    query_data = _flatten_form_data(environ['tiddlyweb.query'])
+    logging.debug(query_data)
+    try:
+        new_bag = Bag(query_data['bag_name'])
+
+        if _bag_exists(new_bag):
+            raise HTTP409('That bag may not be created.')
+
+        username = environ['tiddlyweb.usersign']['name']
+        new_bag.policy.owner = username
+        new_bag.policy.manage = [username]
+        new_bag.desc = query_data['bag_desc']
+
+        for policy_type in ('read', 'write', 'create', 'delete'):
+            texted = query_data.get(policy_type + '_text', None)
+            logging.debug('texted: %s' % texted)
+            if texted:
+                new_bag.policy.__setattr__(policy_type, [x.lstrip().rstrip() for x in texted.split(',')])
+            else:
+                set = query_data[policy_type]
+                new_bag.policy.__setattr__(policy_type, _policy_form_to_entry(username, set))
+
+        store = environ['tiddlyweb.store']
+        store.put(new_bag)
+    except KeyError, exc:
+        raise HTTP400('something went wrong processing for: %s' % exc)
+    return True
+
+
+def _policy_form_to_entry(username, set_name):
+    if set_name == 'private':
+        return [username]
+    if set_name == 'any':
+        return ['ANY']
+    if set_name == 'public':
+        return []
+
+
+def _bag_exists(bag):
+    return False
+
+
+def _flatten_form_data(form_dict):
+    """We assume we only want the first value in any listed data."""
+    query_data = {}
+    for key, value in form_dict.items():
+        query_data[key] = value[0]
+    return query_data
 
 
 def init(config):
