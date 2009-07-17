8ce6888dcf7f3cf8492ac455ff0903ec1b240737
Chris Dent <cdent@peermore.com>
2009-07-17 13:25:13 +0000

    Checking in the formreader plugin created during the avox hackathon.

---
+++ formreader/formreader.py
@@ -0,0 +1,44 @@
+"""
+A quick hack to parse CGI form fields into fields on a 
+tiddler.
+
+TODO:
+
+    better bag name handling
+    ensuring the bag exists
+    redirect to somewhere useful
+
+"""
+
+from tiddlyweb.model.tiddler import Tiddler
+
+from tiddlyweb.web.http import HTTP302
+
+SKIP_FIELDS = [] # add string fields that we don't want in the tiddler data
+TARGET_BAG = 'bag0' # XXX this ought to come from the form. Existence assumed.
+
+
+def init(config):
+    config['selector'].add('/submit', POST=handler)
+
+
+def handler(environ, start_response):
+    input = environ['tiddlyweb.query']
+    store = environ['tiddlyweb.store']
+
+    # deal with python cgi packaging
+    for field in input:
+        if field in SKIP_FIELDS:
+            continue
+        input[field] = input[field][0]
+
+    # need this to come in on the form input
+    tiddler_title = input['tiddler_title']
+
+    tiddler = Tiddler(tiddler_title, TARGET_BAG) # XXX is this the bag you want?
+    tiddler.fields = input
+
+    store.put(tiddler)
+
+    url = '/' # XXX replace with real url
+    raise HTTP302(url) 
