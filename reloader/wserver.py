f13d6be1a1d54877f361e7130c820edc96173a89
Chris Dent <cdent@host49.msm.che.vodafone>
2009-07-21 10:02:00 +0100

    Tidy up and document wserver better, as it is a confusing thing which deserves better.
    Also add a README explaining why there is reloader.py and wserver.py.

--- reloader/wserver.py
+++ reloader/wserver.py
@@ -2,7 +2,7 @@
 A twanager plugin which provides the same job as 
 the reloader.py but uses multiple process to do the
 watching, rather than threads, thus getting around 
-problems on some architectures.
+problems on some architectures (notably OS X).
 """
 
 from tiddlyweb.manage import make_command, server
@@ -22,21 +22,24 @@ def init(config_in):
 
 @make_command()
 def wserver(args):
-    """Run a server that reloads code."""
+    """Run a server that reloads code automagically."""
     poll_interval = config.get('reloader_interval', 1)
     extra_files = config.get('reloader_extra_files', [])
-    pid = os.fork()
-    load_app() # we must call load_app() to process plugins and handlers
+    pid = _start_server()
     if pid:
+        # we must call load_app() to process system_plugins and handlers
+        # and get them into sys.modules, which is what the Monitor
+        # watches.
+        load_app()
         signal.signal(signal.SIGCHLD, sig_child_handler(pid))
         mon = ForkingMonitor(poll_interval=poll_interval, extra_files=extra_files)
         mon.pid = pid
-        mon.args = args
         mon.periodic_reload()
-    _run_server(args)
 
 
 def sig_child_handler(pid):
+    # This tries to make sure we don't leave behind zombies.
+    # BWAINS!!!
     def _sig_child_handler(signum, frame):
         print >> sys.stderr, 'Child server process at %s sent %s.' % (pid, signum)
         try:
@@ -47,19 +50,31 @@ def sig_child_handler(pid):
     return _sig_child_handler
 
 
-def _run_server(args):
-    server(args)
+def _start_server():
+    """
+    Spawn a twanager server process with the same arguments
+    twanager wserver was started with.
+    """
+    args = [sys.executable] + sys.argv
+    args.insert(args.index('wserver'), 'server')
+    args.remove('wserver')
+    pid = os.spawnv(os.P_NOWAIT, sys.executable, args)
+    return pid
 
 
 class ForkingMonitor(Monitor):
+    """
+    Subclass of reloader.Monitor that replaces periodic_reloader
+    with a call to spawn a different server, rather than execing
+    an entire new process.
+    """
 
     def periodic_reload(self):
         while True:
             if not self.check_reload():
+                # We've been told we need to reload.
+                # Kill the child server and start a new one.
                 os.kill(self.pid, signal.SIGTERM)
-                args = [sys.executable] + sys.argv
-                args.insert(args.index('wserver'), 'server')
-                args.remove('wserver')
-                pid = os.spawnv(os.P_NOWAIT, sys.executable, args)
+                pid = _start_server()
                 self.pid = pid
             time.sleep(self.poll_interval)
