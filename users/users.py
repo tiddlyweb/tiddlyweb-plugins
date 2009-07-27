34b360bb9c034bb695d5660170cce5ff16b93bbc
Chris Dent <cdent@rust.lan>
2009-07-27 16:18:22 +0100

    First stab at a stub users plugin which may eventually provide a way to administer users over HTTP.
    This adds /users and /users/{usersign}, accessible by a user with the ADMIN role.
    
    No effort is made to make the output pretty, or yet to accept any input.

---
+++ users/users.py
@@ -0,0 +1,51 @@
+"""
+A plugin that allows for the administration of
+users over HTTP. This is done as a plugin for two
+reasons:
+
+    * To make use of it easily optional for any installation.
+    * To allow exploration of the API before possibly solidifying
+      it in the core.
+
+Basically it allows an admin user to view all the users and
+make changes.
+
+Add 'users' to system_plugins. To view any of these urls
+a user with the ADMIN will need to exist in the store.
+"""
+
+from tiddlywebplugins import require_role
+from tiddlyweb.web.http import HTTP404
+from tiddlyweb.model.user import User
+from tiddlyweb.store import NoUserError
+
+@require_role('ADMIN')
+def list_users(environ, start_response):
+    store = environ['tiddlyweb.store']
+    users = store.list_users()
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['%s\n' % user for user in users]
+
+
+@require_role('ADMIN')
+def get_user(environ, start_response):
+    store = environ['tiddlyweb.store']
+    try:
+        usersign = environ['wsgiorg.routing_args'][1]['usersign']
+        user = User(usersign)
+        user = store.get(user)
+    except (NoUserError, KeyError), exc:
+        raise HTTP404('Unable to load user: %s, %s' % (usersign, exc))
+    start_response('200 OK', [('Content-Type', 'text/plain')])
+    return ['%s' % user]
+
+
+@require_role('ADMIN')
+def put_user(environ, start_response):
+    store = environ['tiddlyweb.store']
+    pass
+
+
+def init(config):
+    config['selector'].add('/users', GET=list_users)
+    config['selector'].add('/users/{usersign}', GET=get_user, PUT=put_user)
