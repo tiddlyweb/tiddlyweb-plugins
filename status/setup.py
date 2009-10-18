425d998a7607a16d1c0de1713f02ae8d0c8f497d
Chris Dent <cdent@oxide.home>
2009-10-18 13:46:06 +0100

    Continue packaging tiddlywebplugins.status
    (forgot makefile and setup.py)

---
+++ status/setup.py
@@ -0,0 +1,18 @@
+
+import os
+from setuptools import setup, find_packages
+from tiddlywebplugins.status import __version__ as VERSION
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.status',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin that provides information on the current user.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.status',
+        packages = find_packages(exclude='test'),
+        author_email = 'cdent@peermore.com',
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools', 'tiddlyweb'],
+        )
