2dd9fc2fa24530fe731da0295b7820bc73d3de82
Chris Dent <cdent@host57.msm.che.vodafone>
2009-10-20 11:50:49 +0100

    Make sure we ignore the twp directory when packaging wikklytextrender.
    Otherwise we end up packaging it in a namespace that doesn't exist.

--- wikklytextrender/setup.py
+++ wikklytextrender/setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.2'
+VERSION = '0.3'
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
@@ -12,7 +12,7 @@ setup(
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = 'Chris Dent',
         url = 'http://pypi.python.org/pypi/tiddlywebplugins.wikklytextrender',
-        packages = find_packages(exclude='test'),
+        packages = find_packages(exclude=['test', 'twp']),
         author_email = 'cdent@peermore.com',
         platforms = 'Posix; MacOS X; Windows',
         install_requires = ['setuptools', 'tiddlyweb', 'wikklytext'],
