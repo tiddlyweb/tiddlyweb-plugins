7714a0287ef0867c67450413825e2b21424fc02b
Chris Dent <cdent@oxide.local>
2009-11-06 13:29:09 +0000

    Create a simple jsonp plugin that subclasses the json serialization to add jsonp support.

---
+++ jsonp/setup.py
@@ -0,0 +1,19 @@
+
+import os
+from setuptools import setup, find_packages
+
+VERSION = '0.1'
+
+setup(
+        namespace_packages = ['tiddlywebplugins'],
+        name = 'tiddlywebplugins.jsonp',
+        version = VERSION,
+        description = 'A TiddlyWeb plugin that provides a jsonp serializer.',
+        long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
+        author = 'Chris Dent',
+        url = 'http://pypi.python.org/pypi/tiddlywebplugins.jsonp',
+        packages = find_packages(exclude='test'),
+        author_email = 'cdent@peermore.com',
+        platforms = 'Posix; MacOS X; Windows',
+        install_requires = ['setuptools', 'tiddlyweb'],
+        )
---
+++ jsonp/tiddlywebplugins/jsonp.py
@@ -0,0 +1,51 @@
+"""
+JSONP serialization for TiddlyWeb.
+
+If jsonp_callback is in tiddlyweb.query, send the results as
+jsonp, otherwise, do the normal JSON results.
+"""
+
+
+from tiddlyweb.serializations.json import Serialization as JSON
+
+SERIALIZERS = {
+        'application/json': ['jsonp', 'application/json; charset=UTF-8'],
+        }
+
+
+def init(config):
+    config['serializers'].update(SERIALIZERS)
+
+
+class Serialization(JSON):
+
+    def _get_jsonp(self):
+        jsonp = self.environ['tiddlyweb.query'].get('jsonp_callback', [None])[0]
+        if jsonp:
+            return '%s(' % jsonp, ')'
+        else:
+            return '', ''
+
+    def list_recipes(self, recipes):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.list_recipes(self, recipes) + suffix
+
+    def list_bags(self, bags):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.list_bags(self, bags) + suffix
+
+    def list_tiddlers(self, bag):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.list_tiddlers(self, bag) + suffix
+
+    def recipe_as(self, recipe):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.recipe_as(self, recipe) + suffix
+
+    def bag_as(self, bag):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.bag_as(self, bag) + suffix
+
+    def tiddler_as(self, tiddler):
+        prefix, suffix = self._get_jsonp()
+        return prefix + JSON.tiddler_as(self, tiddler) + suffix
