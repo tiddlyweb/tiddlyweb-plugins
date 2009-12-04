ce2b0a9235fed3be7ee54f418ca7af74396410f1
Chris Dent <cdent@oxide.lan>
2009-12-04 16:52:22 +0000

    Packaging cleanups for tiddlywebplugins.templates

--- templates/setup.py
+++ templates/setup.py
@@ -2,7 +2,7 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.3'
+VERSION = '0.4'
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
@@ -12,7 +12,7 @@ setup(
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = 'Chris Dent',
         url = 'http://pypi.python.org/pypi/tiddlywebplugins.templates',
-        packages = find_packages(exclude='test'),
+        packages = find_packages(exclude=['test']),
         author_email = 'cdent@peermore.com',
         platforms = 'Posix; MacOS X; Windows',
         install_requires = ['setuptools', 'Jinja2'],
