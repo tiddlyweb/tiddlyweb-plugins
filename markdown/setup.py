a7303f667cf8dd527a048d8c808436168f1c712f
Chris Dent <cdent@oxide.lan>
2009-12-04 16:49:44 +0000

    packaging cleanups.

--- markdown/setup.py
+++ markdown/setup.py
@@ -1,16 +1,13 @@
 
-# YOU NEED TO EDIT THESE
 AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.markdown'
 DESCRIPTION = 'Markdown rendering for TiddlyWeb'
-VERSION = '0.4'
+VERSION = '0.5'
 
 import os
 from setuptools import setup, find_packages
 
-# You should review the below so that it seems correct. install_requires
-# especially.
 setup(
         namespace_packages = ['tiddlywebplugins'],
         name = NAME,
@@ -19,7 +16,7 @@ setup(
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = AUTHOR,
         url = 'http://pypi.python.org/pypi/%s' % NAME,
-        packages = find_packages(exclude='test'),
+        packages = find_packages(exclude=['test']),
         author_email = AUTHOR_EMAIL,
         platforms = 'Posix; MacOS X; Windows',
         install_requires = ['markdown2'],
---
+++ markdown/test/test_wiki_link.py
@@ -0,0 +1,36 @@
+
+from tiddlywebplugins.markdown import render
+from tiddlyweb.model.tiddler import Tiddler
+
+sample = """# Hello
+    
+This is WikiLink
+
+* List
+* List
+"""
+
+sample_linked = """
+
+This is WikiLink and this is not: [NotLink](http://example.com).
+
+"""
+
+def test_no_wiki():
+    tiddler = Tiddler('hello')
+    tiddler.text = sample
+
+    environ = {}
+    output = render(tiddler, environ)
+    assert '<h1>' in output
+    assert '<li>' in output
+    assert 'href' not in output
+
+    environ = {'tiddlyweb.config': {'markdown.wiki_link_base': ''}}
+    output = render(tiddler, environ)
+    assert 'href' in output
+
+    # This is a TODO
+    #tiddler.text = sample_linked
+    #output = render(tiddler, environ)
+    #assert '"NotLink"' not in output
