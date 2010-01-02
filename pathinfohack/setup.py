38e64f459796a9cbf8bdaea3b3d87c7d5e186ef0
Chris Dent <cdent@oxide.local>
2010-01-02 22:32:19 +0000

    Fix short description on pathinfohack.

--- pathinfohack/setup.py
+++ pathinfohack/setup.py
@@ -2,13 +2,13 @@
 import os
 from setuptools import setup, find_packages
 
-VERSION = '0.3'
+VERSION = '0.4'
 
 setup(
         namespace_packages = ['tiddlywebplugins'],
         name = 'tiddlywebplugins.pathinfohack',
         version = VERSION,
-        description = 'A TiddlyWeb plugin providing a union select filter.',
+        description = 'A TiddlyWeb plugin providing support for / in entity names under Apache.',
         long_description=file(os.path.join(os.path.dirname(__file__), 'README')).read(),
         author = 'Oveek',
         url = 'http://pypi.python.org/pypi/tiddlywebplugins.pathinfohack',
