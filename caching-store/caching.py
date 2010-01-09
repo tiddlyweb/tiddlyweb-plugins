457e7632a7d79a6d3be479caccd5d014922aaee6
Chris Dent <cdent@host47.msm.che.vodafone>
2010-01-09 15:46:13 +0000

    In caching.py memcache store, avoid importing config.
    It's import to not import config a bunch of different times, across
    lots of different plugins and stores, but instead use the one provided
    by the environment.

--- caching-store/caching.py
+++ caching-store/caching.py
@@ -5,71 +5,81 @@ from tiddlyweb.store import Store as StoreBoss
 from tiddlyweb.stores import StorageInterface
 from tiddlyweb.model.bag import Bag
 
-from tiddlyweb.config import config
-
 from urllib import quote
 
-try:
-    from google.appengine.api import memcache
-    MC = memcache
-except ImportError:
-    import memcache
-    MC = memcache.Client(config['memcache_hosts'])
-
 class Store(StorageInterface):
+
+    _MC = None
 
     def __init__(self, environ=None):
         if environ is None:
             environ = {}
         self.environ = environ
+        self.config = environ.get('tiddlyweb.config')
+
+        self._mc = self._MC
+
+        if self._mc == None:
+            try:
+                from google.appengine.api import memcache
+                self._MC = memcache
+            except ImportError:
+                import memcache
+                try:
+                    self._MC = memcache.Client(self.config['memcache_hosts'])
+                except KeyError:
+                    from tiddlyweb.config import config
+                    self.config = config
+                    self._MC = memcache.Client(self.config['memcache_hosts'])
+            self._mc = self._MC
 
         internal_environ = {}
         internal_store_environ_config  = {
-                'server_store': config['cached_store'],
+                'server_store': self.config['cached_store'],
                 }
         internal_environ.update(environ)
         internal_environ['tiddlyweb.config'].update(internal_store_environ_config)
-        self.cached_store = StoreBoss(config['cached_store'][0], environ=internal_environ)
-        self.prefix = self.environ['tiddlyweb.config']['server_prefix']
-        self.host = self.environ['tiddlyweb.config']['server_host']['host']
+        self.cached_store = StoreBoss(self.config['cached_store'][0], environ=internal_environ)
+        self.prefix = self.config['server_prefix']
+        self.host = self.config['server_host']['host']
 
     def recipe_delete(self, recipe):
         key = self._recipe_key(recipe)
-        MC.delete(key)
+        self._mc.delete(key)
         self.cached_store.delete(recipe)
 
     def recipe_get(self, recipe):
         key = self._recipe_key(recipe)
-        cached_recipe = _get(key)
+        cached_recipe = self._get(key)
         if cached_recipe:
             recipe = cached_recipe
         else:
             recipe = self.cached_store.get(recipe)
             del recipe.store
-            MC.set(key, recipe)
+            self._mc.set(key, recipe)
         return recipe
 
     def recipe_put(self, recipe):
         key = self._recipe_key(recipe)
         self.cached_store.put(recipe)
-        MC.delete(key)
+        self._mc.delete(key)
 
     def bag_delete(self, bag):
         key = self._bag_key(bag)
-        if MC.delete(key):
-            MC.delete_multi([self._tiddler_key(tiddler) for tiddler in bag.list_tiddlers()])
+        if self._mc.delete(key):
+            self._mc.delete_multi([self._tiddler_key(tiddler) for tiddler in bag.list_tiddlers()])
         self.cached_store.delete(bag)
 
     def bag_get(self, bag):
         if not (hasattr(bag, 'skinny') and bag.skinny):
             key = self._bag_key(bag)
-            cached_bag = _get(key)
+            cached_bag = self._get(key)
             if cached_bag:
                 bag = cached_bag
             else:
                 bag = self.cached_store.get(bag)
                 del bag.store
-                MC.set(key, bag)
+                self._mc.set(key, bag)
         else:
             bag = self.cached_store.get(bag)
             del bag.store
@@ -78,12 +88,12 @@ class Store(StorageInterface):
     def bag_put(self, bag):
         key = self._bag_key(bag)
         self.cached_store.put(bag)
-        MC.delete(key)
+        self._mc.delete(key)
 
     def tiddler_delete(self, tiddler):
         key = self._tiddler_key(tiddler)
-        if MC.delete(key):
-            MC.delete(self._bag_key(Bag(tiddler.bag)))
+        if self._mc.delete(key):
+            self._mc.delete(self._bag_key(Bag(tiddler.bag)))
         self.cached_store.delete(tiddler)
 
     def tiddler_get(self, tiddler):
@@ -91,42 +101,42 @@ class Store(StorageInterface):
             key = self._tiddler_key(tiddler)
         else:
             key = self._tiddler_revision_key(tiddler)
-        cached_tiddler = _get(key)
+        cached_tiddler = self._get(key)
         if cached_tiddler:
             cached_tiddler.recipe = tiddler.recipe
             tiddler = cached_tiddler
         else:
             tiddler = self.cached_store.get(tiddler)
             del tiddler.store
-            MC.set(key, tiddler)
+            self._mc.set(key, tiddler)
         return tiddler
 
     def tiddler_put(self, tiddler):
         key = self._tiddler_key(tiddler)
         self.cached_store.put(tiddler)
-        MC.delete(self._bag_key(Bag(tiddler.bag)))
-        MC.delete(key)
+        self._mc.delete(self._bag_key(Bag(tiddler.bag)))
+        self._mc.delete(key)
 
     def user_delete(self, user):
         key = self._user_key(user)
-        MC.delete(key)
+        self._mc.delete(key)
         self.cached_store.delete(user)
 
     def user_get(self, user):
         key = self._user_key(user)
-        cached_user = _get(key)
+        cached_user = self._get(key)
         if cached_user:
             user = cached_user
         else:
             user = self.cached_store.get(user)
             del user.store
-            MC.set(key, user)
+            self._mc.set(key, user)
         return user
 
     def user_put(self, user):
         key = self._user_key(user)
         self.cached_store.put(user)
-        MC.delete(key)
+        self._mc.delete(key)
 
     def list_recipes(self):
         return self.cached_store.list_recipes()
@@ -168,10 +178,10 @@ class Store(StorageInterface):
         return quote(key.encode('UTF-8'), safe='')
 
 
-def _get(key):
-    object = MC.get(key)
-    if object:
-        logging.debug('cache hit for %s' % key)
-    else:
-        logging.debug('cache miss for %s' % key)
-    return object
+    def _get(self, key):
+        object = self._mc.get(key)
+        if object:
+            logging.debug('cache hit for %s' % key)
+        else:
+            logging.debug('cache miss for %s' % key)
+        return object
