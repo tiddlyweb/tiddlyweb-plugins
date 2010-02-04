b4da3fefb02421176c44a6f06e7bff7e496cb25e
Chris Dent <cdent@oxide.local>
2010-02-04 14:58:09 +0000

    tiddlywebplugins.magicuser: augment the tiddlyweb.usersign
    A tiddler in a special bag is looked up to be used as additional
    information for the user. See the module's docs, code and tests.

---
+++ magicuser/test/test_simple.py
@@ -0,0 +1,58 @@
+
+import shutil
+
+from tiddlyweb.store import Store
+from tiddlyweb.config import config
+
+from tiddlywebplugins.magicuser import Extractor
+
+from tiddlyweb.model.tiddler import Tiddler
+from tiddlyweb.model.bag import Bag
+
+
+def setup_module(module):
+    clean_store()
+    module.environ = {'tiddlyweb.config': config}
+    module.store = Store(config['server_store'][0], config['server_store'][1], module.environ)
+    module.environ['tiddlyweb.store'] = module.store
+
+
+def clean_store():
+    try:
+        shutil.rmtree('store')
+    except:
+        pass # !
+
+
+def test_simple():
+    bag = Bag('MAGICUSER')
+    store.put(bag)
+    extractor = Extractor()
+    userinfo = {'name': 'cdent', 'roles': ['HELLO']}
+
+    more_info = extractor.extract_more_info(environ, userinfo)
+
+    assert 'modifier' in more_info
+    assert 'name' in more_info
+    assert 'roles' in more_info
+    assert 'tags' in more_info
+    assert 'fields' in more_info
+    assert 'modified' in more_info
+
+    assert more_info['tags'] == []
+    assert more_info['fields'] == {}
+    assert more_info['roles'] == ['HELLO']
+
+    tiddler = Tiddler('cdent', 'MAGICUSER')
+    tiddler.fields['roles'] = "GOODBYE CHRISTMAS EASTER ADMIN"
+    tiddler.tags = ['monkey', 'hello', 'barney']
+    tiddler.fields['spaces'] = "one two three"
+    store.put(tiddler)
+
+    more_info = extractor.extract_more_info(environ, userinfo)
+
+    assert more_info['tags'] == ['monkey', 'hello', 'barney']
+    assert len(more_info['roles']) == 5
+    for role in ['HELLO', 'GOODBYE', 'CHRISTMAS', 'EASTER', 'ADMIN']:
+        assert role in more_info['roles']
+    assert more_info['fields']['spaces'] == 'one two three'
---
+++ magicuser/tiddlywebplugins/magicuser.py
@@ -0,0 +1,68 @@
+"""
+A plugin which provides an extractor for TiddlyWeb.
+
+When a user is extracted, after the normal extraction
+has succeeded the user names is looked up as a tiddler
+in the bag defined by 'magicuser.bag' in tiddlywebconfig.
+The default is 'MAGICUSER'.
+
+That tiddler is then expected for attributes and fields to
+add to the tiddlyweb.usersign dictionary which is passed to
+the rest of the TiddlyWeb system (so thus is available to
+plugins and the like).
+
+tiddler.fields['roles'] is parsed for roles in the same manner
+as the tiddler.tags attribute.
+
+See the test file for examples of how the attributes can be 
+used. 
+
+This plugin works by wrapping the existing extactors configured
+for the system. It then calls the wrapped extractors before
+doing its own augmentation.
+"""
+from tiddlyweb.web.extractors import ExtractorInterface
+from tiddlyweb.web.extractor import _try_extractors
+
+from tiddlyweb.store import NoTiddlerError
+from tiddlyweb.model.tiddler import Tiddler, string_to_tags_list
+
+class Extractor(ExtractorInterface):
+
+    def extract(self, environ, start_response):
+        sub_extractors = config['sub_extractors']
+        config['extractors'] = sub_extractors
+
+        # XXX this duplicates from tiddlyweb.web.extractor
+        userinfo = {"name": u'GUEST', "roles": []}
+
+        candidate_userinfo = _try_extractors(environ, start_response)
+
+        if candidate_userinfo:
+            return self._extract_more_info(environ, candiate_userinfo)
+        else:
+            return userinfo
+
+    def extract_more_info(self, environ, userinfo):
+        store = environ['tiddlyweb.store'] 
+        bag_name = environ['tiddlyweb.config'].get('magicuser.bag', 'MAGICUSER')
+        username = userinfo['name']
+        tiddler = Tiddler(username, bag_name)
+        try:
+            tiddler = store.get(tiddler)
+        except NoTiddlerError:
+            pass # tiddler is empty
+        if 'roles' in tiddler.fields:
+            userinfo['roles'].extend(string_to_tags_list(tiddler.fields['roles']))
+            del tiddler.fields['roles']
+        userinfo['fields'] = tiddler.fields
+        userinfo['modifier'] = tiddler.modifier
+        userinfo['modified'] = tiddler.modified
+        userinfo['tags'] = tiddler.tags
+
+        return userinfo
+
+
+def init(config):
+    config['sub_extractors'] = config['extractors']
+    config['extractors'] = ['tiddlywebplugins.magicuser']
