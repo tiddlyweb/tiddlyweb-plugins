942e60c8c6eb88964224134046f4e4e38d7324c6
Chris Dent <cdent@host49.msm.che.vodafone>
2010-03-16 13:33:40 +0000

    Initial commit of tiddlywebplugins.socialuser.
    A way to manage tiddlyweb users over http, in an intranet
    setting.

---
+++ socialusers/setup.py
@@ -0,0 +1,28 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.socialusers'
+DESCRIPTION = 'An HTTP way of accessing and creating TiddlyWeb users.'
+VERSION = '0.9'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude=['test']),
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools',
+        'tiddlyweb',
+        'tiddlywebplugins.users'],
+    zip_safe = False
+    )
