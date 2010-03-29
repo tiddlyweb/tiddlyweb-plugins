62d157786f75f0a8c2e6b04b71bc7a82395a2f4f
Chris Dent <cdent@oxide.local>
2010-03-29 16:47:02 +0100

    Revert "allowing user defined separator"
    This reverts commit 3c987cdced34f62c151446d366c328efc0dbf0ce.
    
    This is just not right.

--- mselect/test/test_union_filter.py
+++ mselect/test/test_union_filter.py
@@ -7,3 +7,4 @@ tiddlers = [Tiddler('1'), Tiddler('c'), Tiddler('a'), Tiddler('b')]
 def test_simple_mselect():
     selected_tiddlers = mselect('title:1,title:c', tiddlers)
     assert ['1','c'] == [tiddler.title for tiddler in selected_tiddlers]
+
--- mselect/tiddlywebplugins/mselect.py
+++ mselect/tiddlywebplugins/mselect.py
@@ -13,11 +13,13 @@ in tiddlywebconfig.py.
 
 from tiddlyweb.filters import FILTER_PARSERS
 from tiddlyweb.filters.select import select_parse
-from tiddlyweb.util import merge_config
-DEFAULT_ENVIRON = {"tiddlyweb.config":{"mselect.separator":","}}
 
-def mselect(command, tiddlers,environ=DEFAULT_ENVIRON):
-    commands = command.split(environ['tiddlyweb.config']['mselect.separator'])
+
+MSELECT_SEPARATOR = ','
+
+
+def mselect(command, tiddlers):
+    commands = command.split(MSELECT_SEPARATOR)
     # un_generate the tiddlers so we can use the list multiple times
     tiddlers = list(tiddlers)
     for command in commands:
@@ -26,11 +28,15 @@ def mselect(command, tiddlers,environ=DEFAULT_ENVIRON):
             yield tiddler
     return
 
+
 def mselect_parse(command):
-    def selector(tiddlers, indexable=False, environ=DEFAULT_ENVIRON):
-        return mselect(command, tiddlers,environ)
+    def selector(tiddlers, indexable=False, environ={}):
+        return mselect(command, tiddlers)
     return selector
-    
+
+
+FILTER_PARSERS['mselect'] = mselect_parse
+
+
 def init(config):
-    merge_config(config,{"mselect.separator":"|"})
-    FILTER_PARSERS['mselect'] = mselect_parse
+    pass
