7122d148552db5e9ed0d27eb38d3bf2279ef707e
Chris Dent <cdent@host57.msm.che.vodafone>
2010-03-30 14:31:47 +0100

    Enclose the mselect filter under init so config is available for setting the separator.
    This is a mess which will go away with fixes in tiddlyweb 1.1.x

--- mselect/tiddlywebplugins/mselect.py
+++ mselect/tiddlywebplugins/mselect.py
@@ -17,30 +17,39 @@ from tiddlyweb.filters.select import select_parse
 
 MSELECT_SEPARATOR = ','
 
-
-def mselect(command, tiddlers, environ=None):
-    if environ == None:
-        environ = {}
-    config = environ.get('tiddlyweb.config', {})
-    separator = config.get('mselect.separator', MSELECT_SEPARATOR)
-    commands = command.split(separator)
-    # un_generate the tiddlers so we can use the list multiple times
-    tiddlers = list(tiddlers)
-    for command in commands:
-        func = select_parse(command)
-        for tiddler in func(tiddlers):
-            yield tiddler
-    return
-
-
-def mselect_parse(command):
-    def selector(tiddlers, indexable=False, environ={}):
-        return mselect(command, tiddlers, environ)
-    return selector
-
-
-FILTER_PARSERS['mselect'] = mselect_parse
-
+# XXX: make the function available for testing.
+# This enclosure mess works around some bugs in
+# control.filter_tiddlers_from_bag which are fixed in 
+# TiddlyWeb 1.1
+test_mselect = None
 
 def init(config):
-    pass
+
+    global test_mselect
+
+    def mselect(command, tiddlers, environ=None):
+        global separator
+        if environ:
+            try:
+                separator = environ['tiddlyweb.config']['mselect.separator']
+            except (TypeError, KeyError):
+                separator = MSELECT_SEPARATOR
+        else:
+            separator = config.get('mselect.separator', MSELECT_SEPARATOR)
+        commands = command.split(separator)
+        # un_generate the tiddlers so we can use the list multiple times
+        tiddlers = list(tiddlers)
+        for command in commands:
+            func = select_parse(command)
+            for tiddler in func(tiddlers):
+                yield tiddler
+        return
+
+    def mselect_parse(command):
+        def selector(tiddlers, indexable=False, environ={}):
+            return mselect(command, tiddlers, environ)
+        return selector
+
+    FILTER_PARSERS['mselect'] = mselect_parse
+
+    test_mselect = mselect
