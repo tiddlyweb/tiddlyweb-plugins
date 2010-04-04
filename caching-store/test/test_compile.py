1dc37e4f845674ac6faea06315ceeb6a27fcc710
Chris Dent <cdent@oxide.local>
2010-04-04 14:11:45 +0100

    Remove caching-store to its own repo
    tiddlyweb/tiddlywebplugins.caching

--- caching-store/test/test_compile.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def test_compile():
-    try:
-        import tiddlywebplugins.caching
-        assert True
-    except ImportError, exc:
-        assert False, exc
--- caching-store/tiddlywebconfig.py
+++
@@ -1,7 +0,0 @@
-# this config is used by the tests
-config = {
-        'server_store': ['tiddlywebplugins.caching', {}],
-        'memcache_hosts': ['127.0.0.1:11211'],
-        'cached_store': ['text', {'store_root': 'store'}],
-        'log_level': 'DEBUG',
-        }
