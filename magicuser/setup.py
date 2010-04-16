89c7726e5985f53afd7b39853d510d1de416b067
Chris Dent <cdent@oxide.local>
2010-04-16 13:31:37 +0100

    Add a README and other structurings to magicuser.

---
+++ magicuser/setup.py
@@ -0,0 +1,27 @@
+AUTHOR = 'Chris Dent'
+AUTHOR_EMAIL = 'cdent@peermore.com'
+NAME = 'tiddlywebplugins.magicuser'
+DESCRIPTION = '"magically" extend tiddlyweb user capabilities'
+VERSION = '0.1'
+
+
+import os
+
+from setuptools import setup, find_packages
+
+
+# You should carefully review the below (install_requires especially).
+setup(
+    namespace_packages = ['tiddlywebplugins'],
+    name = NAME,
+    version = VERSION,
+    description = DESCRIPTION,
+    long_description = open(os.path.join(os.path.dirname(__file__), 'README')).read(),
+    author = AUTHOR,
+    author_email = AUTHOR_EMAIL,
+    url = 'http://pypi.python.org/pypi/%s' % NAME,
+    packages = find_packages(exclude=['test']),
+    platforms = 'Posix; MacOS X; Windows',
+    install_requires = ['setuptools', 'tiddlyweb'],
+    zip_safe = False
+    )
