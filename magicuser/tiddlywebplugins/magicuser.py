badfd3b5cd65ec66e4c26b1d64424fe87839d3bb
Chris Dent <cdent@host46.msm.che.vodafone>
2010-04-20 12:13:54 +0100

    Documentation and pep8, pylint cleanups to magicuser.

--- magicuser/tiddlywebplugins/magicuser.py
+++ magicuser/tiddlywebplugins/magicuser.py
@@ -2,9 +2,17 @@
 A plugin which provides an extractor for TiddlyWeb.
 
 When a user is extracted, after the normal extraction
-has succeeded the user names is looked up as a tiddler
-in the bag defined by 'magicuser.bag' in tiddlywebconfig.
-The default is 'MAGICUSER'.
+has succeeded the user name is looked up in two ways:
+
+1) In the bag defined by 'magiuser.map' in tiddlywebconfig,
+defaulting to 'MAPUSER', look for a tiddler with the title
+the same as the username. If the tiddler exists and there
+is a field named 'mapped_user', the value of that field
+replaces the username extracted.
+
+2) In the bag defined by 'magicuser.bag' in tiddlywebconfig,
+defaulting to 'MAGICUSER', look for a tiddler with the title
+the same as the username.
 
 That tiddler is then inspected for attributes and fields to
 add to the tiddlyweb.usersign dictionary which is passed to
@@ -14,8 +22,8 @@ plugins and the like).
 tiddler.fields['roles'] is parsed for roles in the same manner
 as the tiddler.tags attribute.
 
-See the test file for examples of how the attributes can be 
-used. 
+See the test file for examples of how the attributes can be
+used.
 
 This plugin works by wrapping the existing extactors configured
 for the system. It then calls the wrapped extractors before
@@ -27,14 +35,23 @@ from tiddlyweb.web.extractor import _try_extractors
 from tiddlyweb.store import NoTiddlerError, NoBagError
 from tiddlyweb.model.tiddler import Tiddler, string_to_tags_list
 
+
 class Extractor(ExtractorInterface):
+    """
+    Extract a user information from the HTTP request
+    and then add or change the information about the
+    user using tiddlers stored in some special bags.
+    """
 
     def extract(self, environ, start_response):
+        """
+        Extract user information and return a
+        dictionary of the information.
+        """
         actual_extractors = environ['tiddlyweb.config']['extractors']
         sub_extractors = environ['tiddlyweb.config']['sub_extractors']
         environ['tiddlyweb.config']['extractors'] = sub_extractors
 
-        # XXX this duplicates from tiddlyweb.web.extractor
         userinfo = {'name': u'GUEST', 'roles': []}
 
         candidate_userinfo = _try_extractors(environ, start_response)
@@ -42,7 +59,7 @@ class Extractor(ExtractorInterface):
         environ['tiddlyweb.config']['extractors'] = actual_extractors
 
         if candidate_userinfo:
-            candiate_userinfo['name'] = self.translate_user(environ,
+            candidate_userinfo['name'] = self.translate_user(environ,
                     candidate_userinfo['name'])
             return self.extract_more_info(environ, candidate_userinfo)
         else:
@@ -69,8 +86,9 @@ class Extractor(ExtractorInterface):
         Get more information and attributes about the current
         user from a tiddler named after the user in the bag MAGICUSER.
         """
-        store = environ['tiddlyweb.store'] 
-        bag_name = environ['tiddlyweb.config'].get('magicuser.bag', 'MAGICUSER')
+        store = environ['tiddlyweb.store']
+        bag_name = environ['tiddlyweb.config'].get('magicuser.bag',
+                'MAGICUSER')
         username = userinfo['name']
         tiddler = Tiddler(username, bag_name)
         try:
@@ -78,7 +96,8 @@ class Extractor(ExtractorInterface):
         except (NoTiddlerError, NoBagError):
             pass # tiddler is empty
         if 'roles' in tiddler.fields:
-            userinfo['roles'].extend(string_to_tags_list(tiddler.fields['roles']))
+            userinfo['roles'].extend(string_to_tags_list(
+                tiddler.fields['roles']))
             del tiddler.fields['roles']
         userinfo['fields'] = tiddler.fields
         userinfo['modifier'] = tiddler.modifier
@@ -89,5 +108,8 @@ class Extractor(ExtractorInterface):
 
 
 def init(config):
+    """
+    Initialize the plugin by changing configuration.
+    """
     config['sub_extractors'] = config['extractors']
     config['extractors'] = ['tiddlywebplugins.magicuser']
