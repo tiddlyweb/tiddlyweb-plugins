de3ab6d077d0092612dfababa46d70588e015411
Chris Dent <chris.dent@gmail.com>
2010-06-02 15:46:02 +0100

    Add first (untested) pass of instance definition for prettyerror
    This defines the recipe and bags for the error tiddlers to live in.
    
    No such tiddlers exist, yet.

---
+++ prettyerror/tiddlywebplugins/prettyerror/instance.py
@@ -0,0 +1,52 @@
+
+store_contents = {}
+store_structure = {}
+
+store_contents['_default_errors'] = [
+        'src/_errors/index.recipe',
+        ]
+
+store_structure['recipes'] = {}
+store_structure['bags'] = {}
+
+store_structure['recipes']['_errors'] = {
+        'desc': 'Pretty Errors Error Tiddlers',
+        'recipe': [
+            ('_default_errors', ''),
+            ('_errors', ''),
+            ],
+        'policy': {
+            'read': [],
+            'write': ['R:ADMIN'],
+            'manage': ['R:ADMIN'],
+            'delete': ['R:ADMIN'],
+            'owner': 'administractor',
+            'write': ['R:ADMIN'],
+            },
+        }
+
+store_structure['bags']['_default_errors'] = {
+        'desc': 'Default error tiddlers for Pretty Errors',
+        'policy': {
+            'read': [],
+            'write': ['NONE'],
+            'create': ['NONE'],
+            'delete': ['NONE'],
+            'manage': ['NONE'],
+            'accept': ['NONE'],
+            'owner': 'administrator',
+            },
+        }
+
+store_structure['bags']['_errors'] = {
+        'desc': 'Override error tiddlers for Pretty Errors',
+        'policy': {
+            'read': [],
+            'write': ['R:ADMIN'],
+            'create': ['R:ADMIN'],
+            'delete': ['R:ADMIN'],
+            'manage': ['R:ADMIN'],
+            'accept': ['NONE'],
+            'owner': 'administrator',
+            },
+        }
