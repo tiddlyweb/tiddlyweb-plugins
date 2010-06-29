7a7767833f9c754255d4bcb2f326c1e163081377
Chris Dent <chris.dent@gmail.com>
2010-06-29 14:49:31 +0100

    Since whoosher is using TIDDLER_WRITTEN_HANDLERS it requires tweb 1.1.x

--- whoosh/setup.py
+++ whoosh/setup.py
@@ -15,6 +15,6 @@ setup(
         packages = find_packages(exclude=['test']),
         author_email = 'cdent@peermore.com',
         platforms = 'Posix; MacOS X; Windows',
-        install_requires = ['setuptools', 'tiddlyweb>=0.9.95', 'tiddlywebplugins.utils', 'Whoosh'],
+        install_requires = ['setuptools', 'tiddlyweb>=1.1.0', 'tiddlywebplugins.utils', 'Whoosh'],
         zip_safe=False,
         )
