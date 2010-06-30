551f45de545bba9ebbc56cec73e5ca63a6f73797
Chris Dent <chris.dent@gmail.com>
2010-06-30 13:15:22 +0100

    Get rid of no longer needed scheme in the test tiddlywebconfig.py

--- whoosh/tiddlywebconfig.py
+++ whoosh/tiddlywebconfig.py
@@ -3,7 +3,4 @@ config = {
         'log_level': 'DEBUG',
         'twanager_plugins': ['tiddlywebplugins.whoosher'],
         'system_plugins': ['tiddlywebplugins.whoosher'],
-        'wsearch.schema': dict(title=TEXT, id=ID(stored=True, unique=True),
-            bag=TEXT, text=TEXT, modified=ID, modifier=ID, created=ID,
-            tags=TEXT, field_two=TEXT, hello=TEXT),
         }
