d91444ff0e939996b1652661b798830ba258f9bf
Chris Dent <chris.dent@gmail.com>
2010-07-05 13:49:48 +0100

    Move tiddlywebplugins.whoosher to its own repo.

--- whoosh/tiddlywebconfig.py
+++
@@ -1,8 +0,0 @@
-import mangler
-from whoosh.fields import Schema, ID, KEYWORD, TEXT
-config = {
-        'log_level': 'DEBUG',
-        'twanager.tracebacks': True,
-        'twanager_plugins': ['tiddlywebplugins.whoosher'],
-        'system_plugins': ['tiddlywebplugins.whoosher'],
-        }
