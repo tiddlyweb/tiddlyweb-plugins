f21a59e5f6a2aafdec6c991ff332bb7f1d14223b
Chris Dent <chris.dent@gmail.com>
2010-07-20 09:46:04 +0100

    Move socialusers to its own repo.

--- socialusers/test/test_compile.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def test_compile():
-    try:
-        import tiddlywebplugins.socialusers
-        assert True
-    except ImportError, exc:
-        assert False, exc
--- socialusers/tiddlywebconfig.py
+++
@@ -1,5 +0,0 @@
-import mangler
-config = {
-        'system_plugins': ['tiddlywebplugins.socialusers'],
-        'socialusers.reserved_names': ['badname']
-        }
