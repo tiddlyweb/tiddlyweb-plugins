7c0705c019335f3fb33574f7677bb16745d5275f
Chris Dent <chris.dent@gmail.com>
2010-10-01 11:27:22 +0100

    Revert "Add support for using WikklyText plugins."
    This reverts commit f2b7a0f7dc32cbaf210ddbfc373e150d001fffba.
    
    Conflicts:
    
    	wikklytextrender/setup.py
    
    The changes do not work with calls in the wikitext serialization
    in tiddlywebwiki which deal with wikifying titles and the like.

--- wikklytextrender/tiddlywebplugins/wikklytextrender.py
+++ wikklytextrender/tiddlywebplugins/wikklytextrender.py
@@ -1,23 +1,6 @@
 """
-A TiddlyWiki wiki syntax to HTML renderer for use with 
-TiddlyWeb. Rendering is done using wikklytext.
-
-To use add one or both of the following to tiddlywebconfig.py:
-
-    wikitext.default_renderer': 'wikklytextrender',
-    'wikitext.default_render_map': {
-        'text/x-tiddlywiki': 'wikklytextrender',
-    },
-
-If 'wikklytext.safe_mode' is set to False, HTML within tiddler.text
-that is wrapped in <html> tags will be preserved. If True, the HTML 
-is removed. True is the default.
-
-If 'wikklytext.plugin-dir' is set to a path, that path is used as
-a container of plugins used by WikklyText.
-
-If 'wikklytext.use_wikiwords' is set to False, then CamelCase style
-wikiwords are not rendered. The default is True.
+Render TiddlyWiki syntax wikitext to HTML
+using the WikklyText enginge.
 """
 
 import wikklytext
@@ -41,11 +24,11 @@ def render(tiddler, environ):
     else:
         path = ''
     html = wikitext_to_wikklyhtml('%s/' % server_prefix,
-            path, tiddler, environ)
+            path, tiddler.text, environ)
     return unicode(html, 'utf-8')
 
 
-def wikitext_to_wikklyhtml(base_url, path_url, tiddler, environ):
+def wikitext_to_wikklyhtml(base_url, path_url, wikitext, environ):
     """
     Turn a wikitext into HTML.
     base_url: starting url for links in the wikitext (e.g. '/')
@@ -64,28 +47,17 @@ def wikitext_to_wikklyhtml(base_url, path_url, tiddler, environ):
             pass
         return '%s%s' % (base_url, urllib.quote(url_fragment, safe='')), False
 
+    posthook = PostHook()
 
     safe_mode_setting = environ.get('tiddlyweb.config', {}).get(
             'wikklytext.safe_mode', True)
-    plugindir = environ.get('tiddlyweb.config', {}).get(
-            'wikklytext.plugin-dir', '')
-    use_wikiwords = environ.get('tiddlyweb.config', {}).get(
-            'wikklytext.use_wikiwords', True)
-
-    posthook = PostHook(use_wikiwords)
 
     link_context = {
             '$BASE_URL': '%s%s' % (base_url, path_url),
             '$REFLOW': 0}
-
     try:
-        context = wikklytext.WikContext(plugin_dirs=plugindir,
-                url_resolver=our_resolver)
-        context.environ = environ
-        context.tiddler = tiddler
-        html, newcontext = wikklytext.WikklyText_to_InnerHTML(
-                text=tiddler.text,
-                context=context,
+        html, context = wikklytext.WikklyText_to_InnerHTML(
+                text=wikitext,
                 setvars=link_context,
                 encoding='utf-8',
                 safe_mode=safe_mode_setting,
@@ -102,21 +74,17 @@ class PostHook(object):
     tree with need to link up the wiki words.
     """
 
-    def __init__(self, use_wikiwords=True):
+    def __init__(self):
         # make map of wikiwords
         self.wikiwords = InfiniteDict()
-        self.use_wikiwords = use_wikiwords
 
     def treehook(self, rootnode, context):
         """
         Turn wikiwords into links.
         """
-        if self.use_wikiwords:
-            from wikklytext.wikwords import wikiwordify
-            # add links to any wikiword
-            wikiwordify(rootnode, context, self.wikiwords)
-        else:
-            pass
+        from wikklytext.wikwords import wikiwordify
+        # add links to any wikiword
+        wikiwordify(rootnode, context, self.wikiwords)
 
 
 class InfiniteDict(dict):
