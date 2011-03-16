9e9777e55110bf46d911a3eea6f73ee7285f6f78
FND <fnd@fnd-oda.(none)>
2011-03-16 11:08:58 +0100

    subclassed markdown2 to support custom link labels
    this should only be temporary, provided the following patch is accepted:
    https://github.com/trentm/python-markdown2/pull/53

--- markdown/tiddlywebplugins/markdown.py
+++ markdown/tiddlywebplugins/markdown.py
@@ -44,6 +44,44 @@ class FreeLinker(object):
         return (page, label)
 
 
+# subclass original Markdown class to allow for custom link labels
+# XXX: patch pending: https://github.com/trentm/python-markdown2/pull/53
+g_escape_table = markdown2.g_escape_table
+_hash_text = markdown2._hash_text
+class Markdown(markdown2.Markdown):
+
+    def _do_link_patterns(self, text):
+        link_from_hash = {}
+        for regex, repl in self.link_patterns:
+            replacements = []
+            for match in regex.finditer(text):
+                title = None # XXX: rename variable (ambiguous/misleading)
+                if hasattr(repl, "__call__"):
+                    components = repl(match) # XXX: rename variable
+                    try:
+                        href, title = components
+                    except ValueError:
+                        href = components
+                else:
+                    href = match.expand(repl)
+                replacements.append((match.span(), href, title))
+            for (start, end), href, title in reversed(replacements):
+                escaped_href = (
+                    href.replace('"', '&quot;')  # b/c of attr quote
+                        # To avoid markdown <em> and <strong>:
+                        .replace('*', g_escape_table['*'])
+                        .replace('_', g_escape_table['_']))
+                if not title:
+                    title = text[start:end]
+                link = '<a href="%s">%s</a>' % (escaped_href, title)
+                hash = _hash_text(link)
+                link_from_hash[hash] = link
+                text = text[:start] + hash + text[end:]
+        for hash, link in link_from_hash.items():
+            text = text.replace(hash, link)
+        return text
+
+
 def render(tiddler, environ):
     """
     Render text in the provided tiddler to HTML.
@@ -57,6 +95,6 @@ def render(tiddler, environ):
         ]
     else:
         link_patterns = []
-    processor = markdown2.Markdown(extras=['link-patterns'],
+    processor = Markdown(extras=['link-patterns'],
             link_patterns=link_patterns)
     return processor.convert(tiddler.text)
