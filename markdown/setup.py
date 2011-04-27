4871c73d907cc1beed9ebc77c4331c8b6c4713cb
Chris Dent <chris.dent@gmail.com>
2011-04-27 14:43:31 +0100

    release 0.8 twp.markdown

--- markdown/setup.py
+++ markdown/setup.py
@@ -3,7 +3,7 @@ AUTHOR = 'Chris Dent'
 AUTHOR_EMAIL = 'cdent@peermore.com'
 NAME = 'tiddlywebplugins.markdown'
 DESCRIPTION = 'Markdown rendering for TiddlyWeb'
-VERSION = '0.7'
+VERSION = '0.8'
 
 import os
 from setuptools import setup, find_packages
