bb7f21961d68309dcd1a42727b0bd41c00246c0d
Chris Dent <chris.dent@gmail.com>
2011-09-27 15:50:06 +0100

    [markdown] add an intentionally failing test for wikilinks inside markdown links

--- markdown/test/test_freelinks.py
+++ markdown/test/test_freelinks.py
@@ -1,3 +1,4 @@
+import pytest
 from tiddlywebplugins.markdown import render
 from tiddlyweb.model.tiddler import Tiddler
 
@@ -70,3 +71,12 @@ def test_precedence():
     assert '<a href="FooBar">FooBar</a>' not in output
     assert '<a href="hello FooBar world">...</a>' in output
     assert output == '<p>lorem <a href="hello FooBar world">...</a> ipsum</p>\n'
+
+@pytest.mark.xfail
+def test_precedence_in_markdown_link():
+    tiddler = Tiddler('Foo')
+    environ = { 'tiddlyweb.config': { 'markdown.wiki_link_base': '' } }
+    tiddler.text = 'I see [foo LoremIpsum bar](http://example.org) you'
+    output = render(tiddler, environ)
+    assert output == '<p>I see <a href="http://example.org">foo LoremIpsum bar</a>you</p>'
+
