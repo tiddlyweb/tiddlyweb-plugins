4258e09dca0b6573d94d008b5e612849a21b0777
Chris Dent <chris.dent@gmail.com>
2011-11-12 16:29:42 +0000

    move openid2 to cdent/tiddlywebplugins.openid2

--- openid2/test/test_compile.py
+++
@@ -1,9 +0,0 @@
-
-
-
-def test_compile():
-    try:
-        import tiddlywebplugins.openid2
-        assert True
-    except ImportError, exc:
-        assert False, exc
--- openid2/tiddlywebconfig.py
+++
@@ -1,6 +0,0 @@
-import mangler
-config = {
-    'log_level': 'DEBUG',
-    'auth_systems': ['tiddlywebplugins.openid2'],
-    'server_prefix': '/bonus',
-}
