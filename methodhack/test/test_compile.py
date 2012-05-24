e638a0b969d8c9684e3f1fb1de3c2f4f5f6d5248
Chris Dent <chris.dent@gmail.com>
2012-05-24 15:57:05 +0100

    Move methodhack to its own repo
    https://github.com/cdent/tiddlywebplugins.methodhack

--- methodhack/test/test_compile.py
+++
@@ -1,7 +0,0 @@
-
-def test_compile():
-    try:
-        import tiddlywebplugins.methodhack
-        assert True
-    except ImportError, exc:
-        assert False, exc
--- methodhack/test/test_example.py
+++
@@ -1,3 +0,0 @@
-def test_example():
-    assert True is True
-    assert False is not True
