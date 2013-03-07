84ac071fd95942e77e50ef91907f8ca20cf2e9dc
Chris Dent <chris.dent@gmail.com>
2013-03-07 15:45:15 +0000

    Move migrate to its own repo
    will end up at /cdent/tiddlywebplugins.migrate when done

--- migrate/tiddlywebconfig.py
+++
@@ -1,8 +0,0 @@
-# A default config, make your own changes here.
-import mangler
-config = {
-    'secret': '8cc3f76b875ab6129f193095a629e0c458380578',
-    'twanager_plugins': ['tiddlywebplugins.migrate'],
-# this is the store to which we are migrating things
-    'target_store': ['text', {'store_root': 'newstore'}],
-}
